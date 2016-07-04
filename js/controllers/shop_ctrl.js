bf650a4ef8d63bd1beae34ffa3c1f1d7bbd6f8dd
huiZhengP <hv_zheng@yahoo.fr>
2016-07-04 16:24:36 +0200

    Shop
    modal

--- js/controllers/shop_ctrl.js
+++ js/controllers/shop_ctrl.js
@@ -3,6 +3,7 @@ app.controller('shop_ctrl', function ($scope, shop_facto){
 	$scope.fondCards = {};
 	$scope.selectedIcon = false;
 	$scope.metier = [];
+	$scope.contactEmail = false;
 
 	shop_facto.listFonds
 	.then(function(res) {
