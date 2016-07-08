5c5919dcbd3d6ed845c334492429e99e855d4231
DMDevelopmt <23dm57@gmail.com>
2016-07-08 16:46:43 +0200

    requete cards auth

--- js/factories/cards_facto.js
+++ js/factories/cards_facto.js
@@ -1,8 +1,16 @@
 app.factory("Cards", function($http, $q) {
 	
 	return {
+
 		acceptedCards: function (){
-			return $http.get(ROOT_URL + "/cards/accepted");
-		}
-	};
+			return $q(function(resolve, reject) { 
+				var cards = null;
+				$http.get(ROOT_URL + "/cards/accepted")
+				.success(function(res){
+					console.log(res);
+					resolve(res);
+				});
+		});
+	}
+};
 });
