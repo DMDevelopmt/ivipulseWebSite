cd369badf50964dbe5baded9ea8cd7cbaf7842a1
DMDevelopmt <23dm57@gmail.com>
2016-07-12 10:51:57 +0200

    css

--- js/directives/card.directive.js
+++ js/directives/card.directive.js
@@ -1,7 +1,7 @@
 app.directive('ivicard', function(){
 	return {
 		restrict: 'E',
-		template: "<div class ='card'><img src=''>avatar</img><span>{{card._sender.first_name}}</span><span>{{card._sender.last_name}}</span></div>",
+		template: "<div class ='card background {{card._sender.template}}'><img src=''>avatar</img><br><div>{{card._sender.first_name}}</div><div>{{card._sender.last_name}}</div></div>",
 		scope: {
 			card: '='
 		}
