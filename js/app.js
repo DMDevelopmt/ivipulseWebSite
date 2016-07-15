07daddb6e17e23e4169ad36c975d118e3b24c540
huiZhengP <hv_zheng@yahoo.fr>
2016-07-15 16:22:30 +0200

    shop credit

--- js/app.js
+++ js/app.js
@@ -65,13 +65,12 @@ app.config(['$routeProvider', function($routeProvider) {
  * @param  {[type]} $http)
  * @return {[type]}            [description]
  */
-app.run(['$rootScope', '$location', '$cookies', '$http', 'me',
-    function ($rootScope, $location, $cookies, $http, me) {
+app.run(['$rootScope', '$location', '$cookies', '$http',
+    function ($rootScope, $location, $cookies, $http) {
         // keep user logged in after page refresh
         console.log("Dans app.run()");
 
         window.$rootScope = $rootScope;
-        window.me = $rootScope.me = me;
 
         $rootScope.globals = $cookies.getObject('globals') || {};
 
